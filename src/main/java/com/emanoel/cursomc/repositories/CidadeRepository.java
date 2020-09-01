f720bb75717fb44902b9f6710ccd8452f03c090b
Emanoel Martins <emanoelmartinsv@gmail.com>
2020-09-01 10:05:04 -0300

    Estado e Cidade

---
+++ src/main/java/com/emanoel/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,9 @@
+package com.emanoel.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.emanoel.cursomc.domain.Cidade;
+
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
---
+++ src/main/java/com/emanoel/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.emanoel.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.emanoel.cursomc.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
