110bb5bd5607708ae7b3acabdb8da466cffb873a
Emanoel Martins <emanoelmartinsv@gmail.com>
2020-09-02 22:09:44 -0300

    Pedido, Pagamento, EstadoPagamento

---
+++ src/main/java/com/emanoel/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,9 @@
+package com.emanoel.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.emanoel.cursomc.domain.Pagamento;
+
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer>{
+
+}
---
+++ src/main/java/com/emanoel/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,9 @@
+package com.emanoel.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.emanoel.cursomc.domain.Pedido;
+
+public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
+
+}
