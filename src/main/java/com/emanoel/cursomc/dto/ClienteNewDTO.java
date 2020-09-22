1ad8dda788cb02e69da5d6c2c60eb37bdae105d0
Emanoel Martins <emanoelmartinsv@gmail.com>
2020-09-22 09:59:10 -0300

    Validação customizada: CPF ou CNPJ na inserção de cliente

--- src/main/java/com/emanoel/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/emanoel/cursomc/dto/ClienteNewDTO.java
@@ -2,22 +2,42 @@ package com.emanoel.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import com.emanoel.cursomc.service.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
+	@NotEmpty(message = "Preenchimento Obrigatorio")
+	@Size(min = 3, max = 80, message = "Nome deve ter entre 3 e 80 caracteres")
 	private String nome;
+	
+	@NotEmpty(message = "Preenchimento Obrigatorio")
+	@Email(message = "Email Invalido")
 	private String email;
+	
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	@NotEmpty(message = "Preenchimento Obrigatorio")
 	private String logradouro;
+	
+	@NotEmpty(message = "Preenchimento Obrigatorio")
 	private String numero;
 	private String complemento;
 	private String bairro;
+	
+	@NotEmpty(message = "Preenchimento Obrigatorio")
 	private String cep;
 	
+	@NotEmpty(message = "Preenchimento Obrigatorio")
 	private String telefone1;
+	
 	private String telefone2;
 	private String telefone3;
 	
---
+++ src/main/java/com/emanoel/cursomc/service/validation/ClienteInsertValidator.java
@@ -0,0 +1,39 @@
+package com.emanoel.cursomc.service.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.emanoel.cursomc.domain.enums.TipoCliente;
+import com.emanoel.cursomc.dto.ClienteNewDTO;
+import com.emanoel.cursomc.resources.exceptions.FieldMessage;
+import com.emanoel.cursomc.service.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+
+		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF Invalido"));
+		}
+		
+		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Invalido"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+
+}
