30c0cbadf6d3bee4548c679073e820b49d1f0619
Emanoel Martins <emanoelmartinsv@gmail.com>
2020-09-22 22:53:39 -0300

    validação personalizada para atualizar email, não deixa repetir email, por hora não esta funcionando

--- src/main/java/com/emanoel/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/emanoel/cursomc/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 import com.emanoel.cursomc.domain.Cliente;
+import com.emanoel.cursomc.service.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
