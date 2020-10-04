2a67d7031ebaf52341833315bef5e2fb87b2e5aa
Emanoel Martins <emanoelmartinsv@gmail.com>
2020-10-03 21:31:52 -0300

    Busca de produtos por nome e categoria

---
+++ src/main/java/com/emanoel/cursomc/service/ProdutoService.java
@@ -0,0 +1,36 @@
+package com.emanoel.cursomc.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.emanoel.cursomc.domain.Categoria;
+import com.emanoel.cursomc.domain.Produto;
+import com.emanoel.cursomc.repositories.CategoriaRepository;
+import com.emanoel.cursomc.repositories.ProdutoRepository;
+import com.emanoel.cursomc.service.exceptions.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+	@Autowired
+	private ProdutoRepository repo;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Produto find(Integer id) {
+		Optional<Produto> obj = repo.findById(id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado ID:" + id + ", Tipo: " + Produto.class.getName()));
+	}
+	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return repo.search(nome, categorias, pageRequest);
+	}
+}
