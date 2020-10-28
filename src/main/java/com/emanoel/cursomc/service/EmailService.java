883591b042bb9085cfd859b32f94ea3feffacba4
Emanoel Martins <emanoelmartinsv@gmail.com>
2020-10-28 10:16:04 -0300

    Implementando HTML Email

--- src/main/java/com/emanoel/cursomc/service/EmailService.java
+++ src/main/java/com/emanoel/cursomc/service/EmailService.java
@@ -1,5 +1,7 @@
 package com.emanoel.cursomc.service;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.emanoel.cursomc.domain.Pedido;
@@ -8,4 +10,7 @@ public interface EmailService {
 	
 	void sendOrderConfirmationEmail(Pedido pedido);
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	void sendHtmlEmail(MimeMessage msg);
 }
